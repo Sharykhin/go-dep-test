b1610264d696f0b4b605532b92aa9dd818be830a
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-04-25 15:51:27 +0300

    added dep file

--- main.go
+++ main.go
@@ -4,11 +4,25 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"log"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func init() {
+
+	db, err := sql.Open("mysql", "test:test@tcp(cr-mysql-grpc-server:3306)/test?parseTime=true")
+	if err != nil {
+		log.Fatalf("Could not to open a connection: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Could not ping a database: %v", err)
+	}
+}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/user/me", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
 	}).Methods("GET")
 	log.Fatal(http.ListenAndServe(":5000", r))
