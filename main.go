8b34d78682d97f4ebe61bfebcd946cad34b5eb77
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-04-25 15:04:33 +0300

    some bast

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+	r.HandleFunc("/user/me", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	}).Methods("GET")
+	log.Fatal(http.ListenAndServe(":5000", r))
+
 }
